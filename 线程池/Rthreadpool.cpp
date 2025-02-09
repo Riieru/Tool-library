90c2e68642bac31cbd1038f1d895834382aa09d8
Riieru <136207936+Riieru@users.noreply.github.com>
2025-02-09 13:07:21 +0800

    Add files via upload

---
+++ 线程池/Rthreadpool.cpp
@@ -0,0 +1,179 @@
+﻿#include"Rthreadpool.h"
+
+
+RIIERUTHREAD::Threadpool::Threadpool(unsigned short size) { 
+	_initSize = size; 
+	addThread(size); 
+}
+
+RIIERUTHREAD::Threadpool::~Threadpool(){
+	//关闭线程池
+	{
+		std::lock_guard<std::mutex> lock(_lock);
+		_run = false;
+	}
+
+	_task_cv.notify_all(); // 唤醒所有线程执行
+	for (std::thread& thread : _pool) {
+		//thread.detach(); // 让线程“自生自灭”
+		if (thread.joinable())
+			thread.join(); // 等待任务结束， 前提：线程一定会执行完
+	}
+	_pool.clear();
+}
+
+
+
+
+
+
+
+//添加指定数量的线程
+void RIIERUTHREAD::Threadpool::addThread(unsigned short size) {
+	//线程数自动管理
+	if (threadautograwflag) {
+		//如果线程池停止运行
+		if (!_run)
+			throw std::runtime_error("Grow on ThreadPool is stopped.");
+		std::unique_lock<std::mutex> lockGrow{ lockgrow }; //自动增长锁
+	}
+	for (; _pool.size() < THREADPOOL_MAX_SIZE && size > 0; --size) {
+		//增加线程数量,但不超过 预定义数量 
+		_pool.emplace_back([this] { //工作线程函数
+			// 等待任务或线程池关闭
+			auto wait_condition = [this] {
+				return !_tasks.empty() || !_run;
+				};
+
+			while (true) {
+				//防止 _run==false 时立即结束,此时任务队列可能不为空
+				Task task; // 获取一个待执行的 task
+				while (true) {
+					std::unique_lock<std::mutex> lock{ _lock };
+					// 使用条件变量等待任务，带超时
+					if (threadautograwflag) {
+						if (!_task_cv.wait_for(lock, std::chrono::milliseconds(waittime), [this] {
+							return !_tasks.empty() || !_run;
+							})) {
+							// 超时发生
+							//启动管理器释放方法
+							manageslot->notify_all();
+							if (threadautograwflag && _pool.size() > _initSize) {
+								return; // 自动释放空闲线程
+							}
+						}
+					}
+					else {
+						// 不带超时的等待
+						_task_cv.wait(lock, wait_condition);
+					}
+					// 条件满足（有任务或线程池关闭）
+					if (!_run && _tasks.empty()) {
+						return; // 线程池关闭且任务队列为空，退出线程
+					}
+					if (!_tasks.empty()) {
+						// 取出任务
+						task = std::move(_tasks.front());
+						_tasks.pop();
+						_idlThrNum--; // 减少空闲线程数
+						break; // 退出循环，执行任务
+					}
+				}
+				//执行任务
+				task();
+				{
+					//执行后更改为空闲状态
+					std::unique_lock<std::mutex> lock{ _lock };
+					_idlThrNum++;
+				}
+			  }
+			});//线程对象
+		{
+			//手动添加后，增加的线程
+			std::lock_guard<std::mutex> lock{ _lock };
+			_idlThrNum++;
+		}
+	}
+}//addthread
+
+		//释放线程
+void RIIERUTHREAD::Threadpool::DeleteThread() {
+	// 获取最后一个元素的迭代器
+	auto it = std::prev(_pool.end());
+	std::thread& deletethread = *it;
+
+	if (deletethread.joinable())
+		deletethread.join();
+	_pool.erase(_pool.end());
+	_idlThrNum--;
+}
+
+
+		//构造
+RIIERUTHREAD::RThreadmanage::RThreadmanage(int threadsize) : 
+	//初始化线程池
+	threadpool{new Threadpool(threadsize)} ,
+	//初始化管理线程
+	managethread{std::make_shared<std::thread>(std::bind(&RIIERUTHREAD::RThreadmanage::ThreadManageWork,this))}
+{
+	//传递交互对象
+	threadpool->manageslot = &manage_cv;
+
+}
+RIIERUTHREAD::RThreadmanage::~RThreadmanage() {
+	if (threadpool != NULL) {
+		delete(threadpool);
+		threadpool = NULL;
+	}
+	manage_cv.notify_all();
+	if (managethread->joinable())
+		managethread->join(); // 等待任务结束， 前提：线程一定会执行完
+}
+	
+
+
+
+//管理work
+void RIIERUTHREAD::RThreadmanage::ThreadManageWork() {
+	//管理线程池
+	while (true) {
+
+
+		while (addslot) {
+			std::unique_lock<std::mutex> lock(managemutex);
+			//检测线程池数量 空闲线程多的话，则释放
+			while ((threadpool->_tasks.size() == 0) && (threadpool->_pool.size() > threadpool->_initSize)) {
+				threadpool->DeleteThread();
+			}
+			//打开自动回收模式
+			if (threadpool->threadautograwflag) {
+				//空闲数量大于一定数值，回收  
+				while(threadpool->_run && (threadpool->_tasks.size() == 0) &&(((threadpool->THREADPOOL_MAX_SIZE) * (1-threadpool->threadproportions))< threadpool->_idlThrNum) )
+					threadpool->DeleteThread();
+			}
+
+			//检测线程池是否继续运行
+			if (!this->GetThreadPoolState() || addslot) {
+				return;
+			}
+			manage_cv.wait(lock, [this] {
+				return ((threadpool->_tasks.size() == 0) && (threadpool->_pool.size() > threadpool->_initSize)) || addslot;
+				});
+
+		}
+
+	}
+}
+
+
+
+//设置是否需要释放空闲线程 默认关闭   回收比例默认50%
+RIIERUTHREAD::RThreadmanage* RIIERUTHREAD::RThreadmanage::SetThreadAutoGrawFlag(bool flag, float proportions) {
+	threadpool->threadautograwflag = flag;
+	if (!((proportions < 1) && (proportions > 0)))  proportions = 0.5;
+	threadpool->threadproportions = proportions;
+
+	return this;
+}
+
+
---
+++ 线程池/Rthreadpool.h
@@ -0,0 +1,247 @@
+﻿#pragma once
+
+#include <iostream>
+#include <windows.h>
+#include <vector>
+#include <queue>
+#include <atomic>
+#include <future>
+#include <condition_variable>
+#include <thread>
+#include <functional>
+#include <stdexcept>
+
+
+namespace RIIERUTHREAD {
+
+	class RThreadmanage;
+
+		//线程池
+	class Threadpool {
+
+		friend class RThreadmanage;
+
+	private:
+		unsigned short _initSize;       //初始化线程数量
+		using Task = std::function<void()>; //定义类型
+		std::vector<std::thread> _pool;          //线程池
+		std::queue<Task> _tasks;            //任务队列
+		std::mutex _lock;                   //任务队列同步锁
+
+		std::condition_variable _task_cv;   //条件阻塞
+		std::atomic<bool> _run{ true };     //线程池是否执行
+		std::atomic<int>  _idlThrNum{ 0 };  //空闲线程数量
+
+		//线程自动回收超时时间
+		unsigned int waittime = 30000;
+
+		//线程池增长同步锁
+		std::mutex lockgrow;
+		//线程池自动管理数量设置
+		bool threadautograwflag = false;
+
+
+		//线程池自动回收比例
+		float threadproportions;
+		//线程池最大线程数
+		unsigned int THREADPOOL_MAX_SIZE = 4;
+
+		//线程池关联管理器 (交互对象)
+		std::condition_variable* manageslot;
+
+
+	private:
+
+		Threadpool(unsigned short size = 4);
+
+		~Threadpool();
+
+
+	public:
+		//成员
+		//添加任务  --- 带返回值
+		template<class F, class... Args>
+		auto commit(F&& f, Args&&... args) -> std::future<decltype(f(args...))> {
+
+			if (!_run)  
+				throw std::runtime_error("commit on ThreadPool is stopped.");
+
+			using RetType = decltype(f(args...)); // typename std::result_of<F(Args...)>::type, 函数 f 的返回值类型
+			auto task = make_shared<std::packaged_task<RetType()>>(
+				std::bind(std::forward<F>(f), std::ref(args)...)
+			); // 把函数入口及参数,打包(绑定)
+			std::future<RetType> future_d = task->get_future();
+			{    // 添加任务到队列
+				std::lock_guard<std::mutex> lock{ _lock };//对当前块的语句加锁  lock_guard 是 mutex 的 stack 封装类，构造的时候 lock()，析构的时候 unlock()
+				_tasks.emplace([task]() { // push(Task{...}) 放到队列后面
+					(*task)();
+					});
+			}
+			//线程数自动管理
+			if (threadautograwflag) {
+				if (_idlThrNum < 1 && _pool.size() < THREADPOOL_MAX_SIZE)
+					addThread(1);
+			}
+
+			_task_cv.notify_one(); // 唤醒一个线程执行
+
+			return future_d;
+		}
+
+
+		//无参任务  
+		template<class F>
+		auto commit(F&& f) -> std::future<decltype(f())> {
+
+			if (!_run)    // stoped ??
+				throw std::runtime_error("commit on ThreadPool is stopped.");
+
+			//返回值类型
+			using RetType = decltype(f());
+
+			auto task = std::make_shared<std::packaged_task<RetType()>>(std::forward<F>(f));
+			//返回值
+			std::future<RetType> future_d = task->get_future();
+
+			{
+				//添加任务到队列
+				std::lock_guard<std::mutex> lock{ _lock };
+				_tasks.emplace([task]() {
+					(*task)();
+					});
+			}
+
+			//线程数自动管理
+			if (threadautograwflag) {
+				if (_idlThrNum < 1 && _pool.size() < THREADPOOL_MAX_SIZE)
+					addThread(1);
+			}
+
+			_task_cv.notify_one(); // 唤醒一个线程执行
+
+			return future_d;
+		}
+
+
+
+		//类对象成员函数封装
+		template<class F,class O, class... Args>
+		auto commitobj(F&& f,O&& obj ,Args&&... args) -> std::future<decltype((obj->*f)(args...))> {
+			if (!_run)    // stoped ??
+				throw std::runtime_error("commit on ThreadPool is stopped.");
+
+			using RetType = decltype((obj->*f)(args...)); // typename std::result_of<F(Args...)>::type, 函数 f 的返回值类型
+			auto task = make_shared<std::packaged_task<RetType()>>(
+				std::bind(std::forward<F>(f),std::forward<O>(obj), std::ref(args)...)
+			); // 把函数入口及参数,打包(绑定)
+			std::future<RetType> future_d = task->get_future();
+			{    // 添加任务到队列
+				std::lock_guard<std::mutex> lock{ _lock };
+				_tasks.emplace([task]() { // push(Task{...}) 放到队列后面
+					(*task)();
+					});
+			}
+			//线程数自动管理
+			if (threadautograwflag) {
+				if (_idlThrNum < 1 && _pool.size() < THREADPOOL_MAX_SIZE)
+					addThread(1);
+			}
+
+			_task_cv.notify_one(); // 唤醒一个线程执行
+
+			return future_d;
+		}
+
+
+
+		//添加指定数量的线程
+		void addThread(unsigned short size);
+
+		//释放线程
+		void DeleteThread();
+
+
+	};
+
+
+
+	class RThreadmanage {
+
+		//线程池管理类
+	private:
+		//线程池
+		RIIERUTHREAD::Threadpool* threadpool;
+		//管理线程
+		std::shared_ptr<std::thread> managethread;
+		//线程锁
+		std::mutex managemutex;
+		//线程条件变量
+		std::condition_variable manage_cv;
+		//添加任务信号
+		std::atomic<bool> addslot{ true };
+
+	public:
+		//构造
+		RThreadmanage(int threadsize = 4);
+		~RThreadmanage();
+
+		//管理work
+		void ThreadManageWork();
+
+
+		//功能
+	public:
+		//添加任务  --- 带返回值
+		template<class F, class... Args>
+		auto commit(F&& f, Args&&... args) -> std::future<decltype(f(args...))> {
+			return threadpool->commit(std::forward<F>(f), std::forward<Args>(args)...);
+		}
+
+		//无参任务  
+		template<class F>
+		auto commit(F&& f) -> std::future<decltype(f())> {
+			return threadpool->commit(std::forward<F>(f));
+		}
+
+		//类对象成员函数封装
+		template<class F, class O, class... Args>
+		auto commitobj(F&& f, O&& obj, Args&&... args) -> std::future<decltype((obj->*f)(args...))> {
+			return threadpool->commitobj(std::forward<F>(f), std::forward<O>(obj), std::forward<Args>(args)...);
+		}
+
+	public:
+		//设置是否需要释放空闲线程 默认关闭   回收比例默认50%
+		RThreadmanage* SetThreadAutoGrawFlag(bool flag, float proportions = 0.5);
+
+		//设置释放空闲线程需等待的时间
+		inline RThreadmanage* SetDeleteThreadTime(unsigned int waittime) {
+			threadpool->waittime = waittime;
+			return this;
+		}
+
+		//设置线程池最大线程数  默认4
+		inline RThreadmanage* SetThreadNumMax(unsigned short threadnum) {
+			if (threadnum > 0) threadpool->THREADPOOL_MAX_SIZE = threadnum;
+			return this;
+		}
+
+
+		//返回空闲线程数量
+		inline int GetidlCount() {
+			return threadpool->_idlThrNum;
+		}
+
+		//返回线程数量
+		inline int GetthrCount() {
+			return threadpool->_pool.size();
+		}
+
+		//返回线程池状态
+		inline bool GetThreadPoolState() {
+			return threadpool->_run.load();
+		}
+
+	};
+
+
+}
